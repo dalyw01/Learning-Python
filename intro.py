631c54029f7ae8e65eff06dedeb4407ae73865d4
William Daly <william.daly@bbc.co.uk>
2019-01-05 18:06:24 +0000

    adding some more code from tutorials

--- intro.py
+++ intro.py
@@ -12,6 +12,7 @@ x = 'William'
 y = "Daly"
 z = x + y
 
+
 print(x)
 print(y)
 print (z)
@@ -70,5 +71,100 @@ print(players)
 # Delete list
 players[:] = []
 print(players)
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+age = 27
+
+if age < 27:
+	print("lol")
+
+name = "william"
+
+if name is "william":
+	print("WILLIAM!")
+elif name is "lucy":
+	print("FSDFDSFSFSF")
+else:
+	print("THIS IS AN ELSE")
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+foods = [ "apple" , "bun" , "egg" , "banana" ]
+
+for f in foods:
+	print(f)
+	print(len(f))
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+# 3 times print
+for x in range(3):
+	print("William Daly is cool")
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+# From 3 to 10, print 7 times
+for x in range(3,10):
+	print(x)
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+# Go from 10 to 40 in steps of 5
+for x in range(10,40,5):
+	print(x)
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+buttcrack = 1
+
+while buttcrack < 11:
+	print("This is the buttcrack")
+	buttcrack+=1
+	break
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+derp = 10
+
+for x in range(99):
+	if x == derp:
+		print("Now we're breaking out")
+		break
+	else:
+		print("Need to increment again")
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+numbersTaken = [3,5,7]
+
+# continue means skip past everything but keep iterating in the loop
+for b in range (0,11):
+	if b in numbersTaken:
+		continue
+		print("sfsdfsfsfsf")
+	else:
+		print(b)
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+def loling():
+	print("LOL")
+
+def addOne( new_number ):
+	new_number+=1
+	print(new_number)
+
+loling()
+addOne(7)
+
+print ('- - - - - - - - - - - - - - - - - - -')
+
+def allowedDatingAge( new_age ):
+	return (new_age/2)+7
+
+williams_age = 27
+print("The allowed age for you at " + str(williams_age) + " is " + str(allowedDatingAge(williams_age)))
+
 
 print ('- - - - - - - - - - - - - - - - - - -')
