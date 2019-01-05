72281169b7a05dbfda6d53bfd1d43b4545d3278e
William Daly <william.daly@bbc.co.uk>
2019-01-05 11:35:28 +0000

    adding first file from tutorials

---
+++ intro.py
@@ -0,0 +1,25 @@
+# python into.py
+# Basic file for me to play around and learn the sytnax of Python 3 a little
+
+print ('Hello, world!')
+print ('They said "HELLO"')
+print ('I don\'t think so Bob')
+print (r'Here is the path > C:\Path\To\My\Folder')
+
+print ('- - - - - - - - - - - - - - - - -')
+
+x = 'William'
+y = "Daly"
+z = x + y
+
+print(x)
+print(y)
+print (z)
+print(x + " " + y)
+
+print ('- - - - - - - - - - - - - - - - -')
+
+name = "Tuna McFishy"
+print (name[0])
+print (name[-1])
+print (name[2:5])
